62db79c0cf9aa91a3b4f339ef8686768e2ba2db5
AleksandrV <kolomel@yandex.ru>
2021-06-25 10:48:38 +0300

    upd App & Index, add removeQuestionaire method

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,58 @@
 <template>
   <div id="q-app">
-    <router-view />
     <Index
-    v-bind:questionnaires="questionnaires"
+      v-bind:questionnaires="questionnaires"
+      @remove-questionnaire="removeQuestionnaire"
     />
-    <Form
-      @add-questionnaire="addQuestionnaire"
-    />
+    <Form @add-questionnaire="addQuestionnaire" />
   </div>
 </template>
 <script>
-import Index from './pages/Index.vue'
-import Form from './pages/Form.vue'
+import Index from "./pages/Index.vue";
+import Form from "./pages/Form.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: { Index, Form },
-  data () {
+  data() {
     return {
-      columns: [
-        { name: 'lastName', align: 'center', label: 'Фамилия', field: 'lastName', sortable: true },
-        { name: 'firstName', label: 'Имя', field: 'firstName', sortable: true },
-        { name: 'middleName', label: 'Отчество', field: 'middleName' },
-        { name: 'birthDate', label: 'Дата рождения', field: 'birthDate', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10)},
-        { name: 'description', label: 'Описание', field: 'description' },
-        { name: 'action', label: 'действие', field: 'action' }
-      ],
       questionnaires: [
-
-      ]
-    }
+        {
+          firstName: 1,
+          lastName: 2,
+          middleName: 3,
+          birthDate: 4,
+          description: 5,
+          id: 1,
+        },
+        {
+          firstName: 1,
+          lastName: 2,
+          middleName: 3,
+          birthDate: 4,
+          description: 5,
+          id: 2,
+        },
+        {
+          firstName: 1,
+          lastName: 2,
+          middleName: 3,
+          birthDate: 4,
+          description: 5,
+          id: 3,
+        },
+      ],
+    };
   },
 
   methods: {
-    addQuestionnaire (Questionnaire) {
-     this.questionnaires.push(Questionnaire);
-     console.log(Questionnaire);
+    addQuestionnaire(Questionnaire) {
+      this.questionnaires.push(Questionnaire);
+      console.log(Questionnaire);
     },
-  }
-}
+    removeQuestionnaire(id) {
+      this.questionnaires = this.questionnaires.filter(el => el.id !== id)
+    },
+  },
+};
 </script>
-
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,63 +1,83 @@
 <template>
   <div class="q-pa-md">
-    <router-link to="/questionnaries" exact><q-btn color="white" text-color="black" label="Добавит анкету" /></router-link>
+    <router-link to="/questionnaries" exact
+      ><q-btn color="white" text-color="black" label="Добавит анкету"
+    /></router-link>
     <q-table
       title="Treats"
-      dense
-      :data="data"
+      :rows="questionnaires"
       :columns="columns"
       row-key="name"
     >
-     <template v-slot:body="props">
-        <q-tr :id="props.row.id" v-on:click="$emit('removeQuestionnaire')" :props="props">
-          <q-td key="lastName" :props="props">
-              {{ props.row.lastName }}
+
+      <template v-slot:header="props">
+        <q-tr :props="props">
+          <q-th v-for="col in props.cols" :key="col.name" :props="props">
+            {{ col.label }}
+          </q-th>
+        </q-tr>
+      </template>
+
+      <template v-slot:body="props">
+        <q-tr v-bind:id="props.row.id" :props="props">
+          <q-td
+            key="lastName"
+            :props="props"
+          >
+            {{ props.row.lastName }}
           </q-td>
           <q-td key="firstName" :props="props">
-              {{ props.row.firstName }}
+            {{ props.row.firstName }}
           </q-td>
           <q-td key="middleName" :props="props">
-              {{ props.row.middleName }}
+            {{ props.row.middleName }}
           </q-td>
           <q-td key="birthDate" :props="props">
-              {{ props.row.birthDate }}
+            {{ props.row.birthDate }}
           </q-td>
           <q-td key="description" :props="props">
-              {{ props.row.description }}
+            {{ props.row.description }}
           </q-td>
-          <q-td key="action" :props="props">
-              <q-icon name="mode_edit" />
-              <q-icon name="delete" />
+          <q-td v-bind:id="props.row.id" key="action" :props="props">
+            <!-- <q-icon v-on:click="removeQuestionnaire" name="mode_edit" /> -->
+            <q-icon v-on:click="$emit('remove-questionnaire', props.row.id)" name="delete" />
           </q-td>
         </q-tr>
       </template>
-      </q-table>
+    </q-table>
   </div>
 </template>
 
 <script>
+
+const columns = [
+  {
+    name: "lastName",
+    align: "center",
+    label: "Фамилия",
+    field: "lastName",
+    sortable: true,
+  },
+  { name: "firstName", label: "Имя", field: "firstName", sortable: true },
+  { name: "middleName", label: "Отчество", field: "middleName" },
+  {
+    name: "birthDate",
+    label: "Дата рождения",
+    field: "birthDate",
+    sortable: true,
+    sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
+  },
+  { name: "description", label: "Описание", field: "description" },
+  { name: "action", label: "действие", field: "action" },
+];
+
 export default {
   props: ['questionnaires'],
-  data () {
+  setup() {
     return {
-      selected: [],
-      columns: [
-        { name: 'lastName', align: 'center', label: 'Фамилия', field: 'lastName', sortable: true },
-        { name: 'firstName', label: 'Имя', field: 'firstName', sortable: true },
-        { name: 'middleName', label: 'Отчество', field: 'middleName' },
-        { name: 'birthDate', label: 'Дата рождения', field: 'birthDate', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10)},
-        { name: 'description', label: 'Описание', field: 'description' },
-        { name: 'action', label: 'действие', field: 'action' }
-      ],
-      data: this.questionnaires
-    }
+      columns,
+    };
   },
+};
 
-  methods: {
-    removeQuestionnaire (evt) {
-      console.log(evt.currentTarget.id);
-    }
-  }
-}
 </script>
-
